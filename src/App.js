cf26c8b76dd0316e719d434b139f4994d80613c1
Bhuvana-1029 <hariprasath200815@gmail.com>
2025-03-01 14:04:25 +0530

    'login'

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import Login from './Login'
+import Login from './Login';
+import Products from './Products';
 import Home from './Home';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-const App=()=> {
+
+function App() {
   return (
- // <div className="App"> 
-       <BrowserRouter>
-     <Routes>
-        
-         <Route path='/Home' element={<Home/>}/>
-         <Route path='/home' element={<Home/>}/>
-       </Routes>
-       </BrowserRouter>
-  
+    <div className="App"> 
+        <BrowserRouter>
+        <Routes>
+          <Route path='' element={<Login/>}/>
+          <Route path='/Login' element={<Login/>}/>
+          <Route path='/Products' element={<Products/>}/> 
+          <Route path='/Home' element={<Home/>}/>
+        </Routes>
+        </BrowserRouter>
+    </div>
   );
-};
+}
 
 export default App;
-// import React from 'react';
-// import './App.css';
-// import Login from './login';
-
-// function App() {
-//   return (
-//     <Login />
-//   );
-// }
-
-//export default App;
